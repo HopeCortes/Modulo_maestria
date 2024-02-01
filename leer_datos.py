07867ecbe184a5bd9a00a85656cf4913fca5ada4
hopecortesv <31742857-hopecortesv@users.noreply.replit.com>
2024-02-01 00:58:05 +0000

    modulo_maestria

--- leer_datos.py
+++ leer_datos.py
@@ -1,10 +1,19 @@
 import sqlite3
 
+
 def leer_datos():
   conn = sqlite3.connect("censo.db")
   cursor = conn.cursor()
 
   cursor.execute("SELECT * FROM censo LIMIT 10")
 
-# Obtener y mostrar los datos
-row = cursor.fe
+  # Obtener y mostrar los datos
+  rows = cursor.fetchall()
+  for row in rows:
+    print(row)
+
+  conn.close()
+
+
+if __name__ == '__main__':
+  leer_datos()
